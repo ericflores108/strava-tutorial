8a1ca6871b607772f65506c8b2578e8628c01307
Eric Flores <95382086+ericflores108@users.noreply.github.com>
2023-03-12 09:16:01 -0700

    Initial commit

---
+++ test/strava-tutorial.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StravaTutorial from '../lib/strava-tutorial-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/strava-tutorial-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StravaTutorial.StravaTutorialStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
